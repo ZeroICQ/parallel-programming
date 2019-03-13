2545e451cee688815b7b0ba29098780592765e93
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-03-14 02:18:38 +1000

    its 2:18 and I am making plots with shrek
    meh...

--- docs/js/task3.js
+++ docs/js/task3.js
@@ -1,6 +1,6 @@
-var SpeedFromThreads1BillionElement = document.getElementById("SpeedFromThreads1Billion");
+var SpeedFromThreads05BillionElement = document.getElementById("SpeedFromThreads1Billion");
 
-var SpeedFromThreads1Billion = new Chart(SpeedFromThreads1BillionElement, {
+var SpeedFromThreads05Billion = new Chart(SpeedFromThreads05BillionElement, {
     type: "horizontalBar",
     options: {
         title: {
@@ -19,8 +19,68 @@ var SpeedFromThreads1Billion = new Chart(SpeedFromThreads1BillionElement, {
 
 });
 
+
+
+var kekImg = new Image();
+kekImg.src = "https://iknowyourmeme.files.wordpress.com/2016/04/14280761127830.jpg?w=616"
+kekImg.onload = function() {
+    var SpeedFromThreads1MillionElement = document.getElementById("SpeedFromThreads1Billion");
+    var fillPattern = SpeedFromThreads1MillionElement.getContext("2d").createPattern(kekImg, 'repeat');
+
+    var SpeedFromThreads1Billion = new Chart(SpeedFromThreads1MillionElement, {
+        type: "horizontalBar",
+        options: {
+            title: {
+                display: true,
+                text: "16^6",
+            },
+            legend: {
+                display: false,
+            },
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'время, сек'
+                    }
+                }]
+            }
+        },
+
+        data: {
+            labels: ["1 thread", "2 threads", "3 threads", "4 threads",
+                     "5 threads", "6 threads", "7 threads", "8 threads"],
+             datasets: [{
+                label:"время",
+                data: [
+                    0.00267730897758156,
+                    0.00177013804204762,
+                    0.00142262596637011,
+                    0.00108176295179874,
+                    0.00096493796445429,
+                    0.00089244905393571,
+                    0.00087713601533324,
+                    0.00075812509749085
+                ],
+                backgroundColor: fillPattern,
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1
+
+             }]
+        },
+
+    });
+
+
+};
+
+
+
 var spongeBobImg = new Image();
-spongeBobImg.src = "https://www.soyhomosensual.com/wp-content/uploads/2017/12/spongebob-gay.jpg"
+spongeBobImg.src = "https://pixel.nymag.com/imgs/daily/selectall/2018/05/11/rainbow/imagination.w700.h700.jpg"
 spongeBobImg.onload = function() {
     var SpeedFromThreads1MillionElement = document.getElementById("SpeedFromThreads1Million");
     var fillPattern = SpeedFromThreads1MillionElement.getContext("2d").createPattern(spongeBobImg, 'repeat');
@@ -30,7 +90,7 @@ spongeBobImg.onload = function() {
         options: {
             title: {
                 display: true,
-                text: "16^6",
+                text: "10^6",
             },
             legend: {
                 display: false,
