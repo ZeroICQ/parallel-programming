1dff0d12b7a12c86ed5f5646348c3dbad4e636bd
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-03-14 02:03:28 +1000

    data for 10^6 length

--- docs/_reports/task3.md
+++ docs/_reports/task3.md
@@ -3,7 +3,10 @@ title: Задание 3
 jsarr:
     - /js/task3.js
 ---
-# Скалярное произведение
 
 <!--more-->
-<canvas id="myChart" width="400" height="400"></canvas>
+Длина вектора 10^9
+<canvas id="SpeedFromThreads1Billion" width="400" height="400"></canvas>
+
+Длина вектора 10^6
+<canvas id="SpeedFromThreads1Million" width="400" height="400"></canvas>
--- docs/js/task3.js
+++ docs/js/task3.js
@@ -1,37 +1,76 @@
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
+var SpeedFromThreads1BillionElement = document.getElementById("SpeedFromThreads1Billion");
+
+var SpeedFromThreads1Billion = new Chart(SpeedFromThreads1BillionElement, {
+    type: "horizontalBar",
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
+        title: {
+            display: true,
+            text: "title",
         }
+    },
+
+    data: {
+        labels: ["1 thread", "2 threads", "3 threads", "4 threads", 
+                 "4 threads", "5 threads", "6 threads", "7 threads", "8 threads"],
+         datasets: [{
+            data: []
+         }]
     }
+
 });
+
+var spongeBobImg = new Image();
+spongeBobImg.src = "https://www.soyhomosensual.com/wp-content/uploads/2017/12/spongebob-gay.jpg"
+spongeBobImg.onload = function() {
+    var SpeedFromThreads1MillionElement = document.getElementById("SpeedFromThreads1Million");
+    var fillPattern = SpeedFromThreads1MillionElement.getContext("2d").createPattern(spongeBobImg, 'repeat');
+
+    var SpeedFromThreads1Billion = new Chart(SpeedFromThreads1MillionElement, {
+        type: "horizontalBar",
+        options: {
+            title: {
+                display: true,
+                text: "16^6",
+            },
+            legend: {
+                display: false,
+            },
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'время, сек'
+                    }
+                }]
+            }
+        },
+
+        data: {
+            labels: ["1 thread", "2 threads", "3 threads", "4 threads",
+                     "5 threads", "6 threads", "7 threads", "8 threads"],
+             datasets: [{
+                label:"время",
+                data: [
+                    0.00267730897758156,
+                    0.00177013804204762,
+                    0.00142262596637011,
+                    0.00108176295179874,
+                    0.00096493796445429,
+                    0.00089244905393571,
+                    0.00087713601533324,
+                    0.00075812509749085
+                ],
+                backgroundColor: fillPattern,
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1
+
+             }]
+        },
+
+    });
+
+
+}
