1143fe703c74be409925c3aab8bfd87741382cfe
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-03-28 16:35:42 +1000

    WIP

--- src/openmpi/task7_mpi/main.cpp
+++ src/openmpi/task7_mpi/main.cpp
@@ -6,20 +6,24 @@ using namespace std;
 
 void bandwidth(int length, int rank) {
     auto message = new char[length];
-    auto N = 100;
+    auto N = 10;
     double elapsed_time = 0;
 
     for (auto i = 0; i < N; i++) {
-        auto start_time = MPI_Wtime();
-
         if (rank == 0) {
+            auto start_time = MPI_Wtime();
             MPI_Send(message, length, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
-            MPI_Recv(message, length, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(message, length, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            elapsed_time += (MPI_Wtime() - start_time);
         }
         else if (rank == 1) {
-            MPI_Recv(message, length, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Send(message, length, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+            MPI_Recv(message, length, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(message, length, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
         }
+    }
+
+    if (rank == 0) {
+        cout << elapsed_time/N << endl;
     }
 
     delete[] message;
@@ -32,10 +36,7 @@ int main(int argc, char **argv)
     int rank, size;
 //    MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-
-
-
+    bandwidth(1024*1024*800, rank);
 
     MPI_Finalize();
     return EXIT_SUCCESS;
