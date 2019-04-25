07267197d1908d19cc4264e61cd8ad1959472cbf
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-04-25 14:54:33 +0000

    17 save portname to file

--- src/openmpi/task17_mpi/client.cpp
+++ src/openmpi/task17_mpi/client.cpp
@@ -5,16 +5,21 @@ using namespace std;
 
 int main(int argc, char **argv) {
     int rank;
-//    cout << "rank is " << rank << endl;
     MPI_Init(&argc, &argv);
     char port_name[MPI_MAX_PORT_NAME];
 
     MPI_Comm intercomm;
-    strcpy(port_name, argv[1]);
-//    cout << "portname is " << port_name << endl;
+    MPI_File  fh;
+
+    MPI_File_open(MPI_COMM_WORLD, "portname.txt", MPI_MODE_RDONLY , MPI_INFO_NULL, &fh);
+    MPI_File_set_view(fh, 0, MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
+    MPI_File_read(fh, port_name, MPI_MAX_PORT_NAME, MPI_CHAR, MPI_STATUS_IGNORE);
+
+//    strcpy(port_name, argv[1]);
 
     MPI_Comm_connect(port_name, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    rank = 10;
     MPI_Send(&rank, 1, MPI_INT, 0, 0, intercomm);
 
     MPI_Finalize();
--- src/openmpi/task17_mpi/server.cpp
+++ src/openmpi/task17_mpi/server.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 //server
 int main(int argc, char **argv) {
-    int r;
+    int r, rank;
     MPI_Init(&argc, &argv);
 
     char port_name[MPI_MAX_PORT_NAME];
@@ -13,16 +13,29 @@ int main(int argc, char **argv) {
     MPI_Status status;
     MPI_Comm intercomm;
     MPI_Open_port(MPI_INFO_NULL, port_name);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_File fh;
 
     printf("portname: %s\n", port_name);
+    if (rank == 0) {
+        cout << "write to file" << endl;
+        MPI_File_open(MPI_COMM_WORLD, "portname.txt", MPI_MODE_CREATE | MPI_MODE_WRONLY | MPI_MODE_DELETE_ON_CLOSE, MPI_INFO_NULL, &fh);
+        MPI_File_write(fh, &port_name, strlen(port_name), MPI_CHAR, MPI_STATUS_IGNORE);
+    }
+    cout << "Waiting for client..." << endl;
 
     MPI_Comm_accept(port_name, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm);
+    cout << "Client connected.." << endl;
+
     MPI_Recv(&r, 1, MPI_INT, 0, 0, intercomm, &status);
 
+    if (rank == 0) {
+        MPI_File_close(&fh);
+    }
     MPI_Comm_free(&intercomm);
     MPI_Close_port(port_name);
 
-    printf("Клиент отправил %d\n", r);
+    printf("Client send %d\n", r);
     MPI_Finalize();
 
     return EXIT_SUCCESS;
